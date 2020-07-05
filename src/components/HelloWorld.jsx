dcf13fdd369ad3bc6de5f088fb412e10a63b08ba
tony <effrenanthony2704@gmail.com>
2020-07-05 14:44:31 -0500

    ReactClase2-Slint0

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import '../assets/styles/App.scss'
 
-const HelloWorld = () =>{
-    <h1>Hola mundo</h1>
-};
+const HelloWorld = () => {
+    return (
+        <h1>Hola mundo</h1>
+    )
+}
+
+// const HelloWorld = () => (
+//     <h1>Hola mundo</h1>
+// )
 
 export default HelloWorld
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom'
 import HelloWorld from './components/HelloWorld'
 ReactDOM.render(
     <HelloWorld></HelloWorld>
-    , document.getElementById(app)
+    , document.getElementById('app')
 )
