a2b28090fb514798fe9661c7efda18638f62b117
tony <effrenanthony2704@gmail.com>
2020-07-10 22:54:40 -0500

    addRedux

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+// children es una palabra reservada para layout
+const Layout = ({ children }) => (
+  <div className="App">
+    <Header />
+    {children}
+    <Footer />
+  </div>
+);
+
+export default Layout;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,33 +1,40 @@
 /* eslint-disable arrow-parens */
 /* eslint-disable implicit-arrow-linebreak */
 import React from 'react';
-import Header from '../components/Header';
+import { connect } from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
-import useInitialState from '../hoooks/useInitialState';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+// import useInitialState from '../hoooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initalState';
+// const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+// le traidgo los elementos que tengo como props
+const Home = ({ myList, trends, originals }) => {
 
-  const initialState = useInitialState(API);
+  // const initialState = useInitialState(API);
 
+  // return initialState.length === 0 ? <h1>Loading...</h1> (
   return (
-    <div className="App">
-      <Header />
+    <>
       <Search />
 
       {
         // si existe my list y ademas la logitud es mayor que 0, pintalo
-        initialState.mylist.length > 0 && (
+        myList.length > 0 && (
           // eslint-disable-next-line react/jsx-wrap-multilines
           <Categories title="Mi lista">
             <Carousel>
-              <CarouselItem />
+              {/* recocrre con map los items del json trends, el key es el item id y trae los props de la destruccturacion de item en el jason */}
+              {myList.map(item =>
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                <CarouselItem key={item.id} {...item} />,
+              // eslint-disable-next-line function-paren-newline
+              )}
             </Carousel>
           </Categories>
         )
@@ -36,7 +43,7 @@ const App = () => {
       <Categories title="Tendencias">
         <Carousel>
           {/* recocrre con map los items del json trends, el key es el item id y trae los props de la destruccturacion de item en el jason */}
-          {initialState.trends.map(item =>
+          {trends.map(item =>
             // eslint-disable-next-line react/jsx-props-no-spreading
             <CarouselItem key={item.id} {...item} />,
           // eslint-disable-next-line function-paren-newline
@@ -45,16 +52,24 @@ const App = () => {
       </Categories>
       <Categories title="Originales">
         <Carousel>
-          {initialState.originals.map(item =>
+          {originals.map(item =>
             // eslint-disable-next-line react/jsx-props-no-spreading
             <CarouselItem key={item.id} {...item} />,
             // eslint-disable-next-line function-paren-newline
           )}
         </Carousel>
       </Categories>
-      <Footer />
-    </div>
+    </>
   );
 };
+// export default connect(props, actions)(Home);
+// llamo los elementos que vamos a trabajar
+const mapStateToProps = state => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
 
-export default App;
+export default connect(mapStateToProps, null)(Home);
